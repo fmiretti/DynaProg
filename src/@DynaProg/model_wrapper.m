a064339af86d62bb2147c4633aa10499796da03f
Federico Miretti <federico.miretti@polito.it>
2022-05-03 11:48:43 +0200

    Revert "refactor: move model output check to model_wrapper"
    This reverts commit fb530cd666d5f116a438c965520ec4e5afa94c85.

--- src/@DynaProg/model_wrapper.m
+++ src/@DynaProg/model_wrapper.m
@@ -1,13 +1,10 @@
 function [states_next, stageCost, unFeas, addOutputs] = model_wrapper(obj, state, control, exoInput, IntermediateVars)
-
 getAddOuts = nargout > 3;
 if getAddOuts
     addOutputs = cell(1, obj.NumAddOutputs);
 end
-
-%% Run the function model
+% Run the function model
 if obj.UseSplitModel
-    % Run the internal function model
     if ~getAddOuts && ~obj.UseExoInput
         [states_next, stageCost, unFeas] = obj.SysNameInt(state, control, IntermediateVars);
         addOutputs = {};
@@ -20,7 +17,6 @@ if obj.UseSplitModel
         [states_next, stageCost, unFeas, addOutputs{:}] = obj.SysNameInt(state, control, exoInput, IntermediateVars);
     end
 else
-    % Run the function model
     if ~getAddOuts && ~obj.UseExoInput
         [states_next, stageCost, unFeas] = obj.SysName(state, control);
         addOutputs = {};
@@ -33,21 +29,7 @@ else
         [states_next, stageCost, unFeas, addOutputs{:}] = obj.SysName(state, control, exoInput);
     end
 end
-
-% Set unfeas for unconstrained problems
 if isempty(unFeas)
     unFeas = zeros(size(state{1}));
 end
-
-%% Model output checks
-if k == obj.Nstages
-    if iscell(stageCost)
-        error('DynaProg:wrongFormatStageCost', ['The stage '...
-            'cost must be returned as a numeric type, not a cell.'])
-    elseif ~isnumeric(stageCost)
-        error('DynaProg:wrongFormatStageCost', ['The stage '...
-            'cost must be returned as a numeric type.'])
-    end
-end
-
 end
