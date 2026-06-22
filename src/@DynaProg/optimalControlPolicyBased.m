bd54fa45069de5f9fe6ad1eee2c9ec21e035f8aa
Federico Miretti <federico.miretti@polito.it>
2026-06-22 09:49:24 +0200

    feat: update policy-based algo for split models

--- src/@DynaProg/optimalControlPolicyBased.m
+++ src/@DynaProg/optimalControlPolicyBased.m
@@ -1,14 +1,20 @@
-function [obj, cv_opt, intVars_opt] = optimalControlPolicyBased(obj, k, state, intVars)
+function [obj, cv_opt, intVars_opt] = optimalControlPolicyBased(obj, k, state, ~)
 %optimalControlPolicyBased find optimal controls for the current stage
 %   using control maps
 
 % Get the CVs from the control map
-cv_opt =  cellfun(@(x) x( state{:} ), obj.ControlMap(:,k), 'UniformOutput', false);
+cv_opt = cellfun(@(x) x(state{:}), obj.ControlMap(:,k), 'UniformOutput', false);
 
-% Extract the intermediate variables for the optimal cv
+% Extract the intermediate variables at the optimal cv.
+% SysNameExt is called at cv_opt directly so that off-grid values produced
+% by linear interpolation of a continuous ControlMap are handled correctly.
 if obj.UseSplitModel
-    intVars = cellfun(@(x) x .* ones(size(cost)), intVars, 'UniformOutput', false);
-    intVars_opt = cellfun(@(x) x(index_opt), intVars, 'UniformOutput', false);
+    if obj.UseExoInput
+        exoInput = num2cell(obj.ExogenousInput(k,:));
+    else
+        exoInput = [];
+    end
+    [intVars_opt, ~] = obj.SysNameExt(cv_opt, exoInput);
 else
     intVars_opt = [];
 end
