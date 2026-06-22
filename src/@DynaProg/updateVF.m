bdd639b1bdd6f1a5e07fb404a430cb153f1d26ae
Federico Miretti <federico.miretti@polito.it>
2026-06-22 15:55:28 +0200

    fix:  enforce U^R restriction in Level-Set value function update

--- src/@DynaProg/updateVF.m
+++ src/@DynaProg/updateVF.m
@@ -12,7 +12,7 @@ if obj.UseLevelSet
     % Check if the set of reachable cvs U^R(x_k) (cvs that
     % lead to a reachable state) is empty. Note: U^R(x_k) is
     % a subset of U(x_k) (set of feasible cvs).
-    isempty_UR = LevelSetValue>0;
+    isempty_UR = LevelSetValue > 0;
     % Level set failure
     if obj.DisplayWarnings && all( LevelSetValue(:) > 0 )
         obj.failedBackward = k;
@@ -28,8 +28,17 @@ end
 % Read VF(k+1)
 VF_next =  obj.VF{k+1}(states_next{:});
 cost = stageCost + VF_next;
-% Set cost-to-go to inf for the unfeasible/unreachable cvs
+% Set cost-to-go to inf for the unfeasible/unreachable CVs
 cost(unfeas) = obj.myInf;
+
+% Level set: restrict the cost-to-go minimization to U^R(x_k)
+if obj.UseLevelSet
+    % Store the cost-to-go at the level-set-minimizing CV
+    cost_MinLevelSetCV = cost(MinLevelSetCV);
+    % Set cost-to-go to inf for the unreachable CVs
+    cost(LevelSet_next > 0) = obj.myInf;
+end
+
 % Find optimal control and corresponding cost (as a function of the current
 % state)
 [cost_opt, cv_opt_idx] = obj.minfun(cost, vecdim_cv);
@@ -38,8 +47,7 @@ cost(unfeas) = obj.myInf;
 if obj.UseLevelSet
     % For those state grid points where no feasible cv was found
     % (isempty_UR), calculate the VF based on the cv that minimizes
-    % the level-set function.
-    cost_MinLevelSetCV = cost(MinLevelSetCV);
+    % the level-set function (eq. 21).
     cost_opt(isempty_UR) = cost_MinLevelSetCV(isempty_UR);
     if obj.StoreControlMap
         cv_opt_idx(isempty_UR) = MinLevelSetCV(isempty_UR);
